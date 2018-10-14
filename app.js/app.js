c486bc521b6cce618c194f37d12d8109aa3630fe
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-13 14:33:59 -1000

    even closer

--- app.js/app.js
+++ app.js/app.js
@@ -4,5 +4,16 @@ darthVader.send();
 
 darthVader.addEventListener('load', function (){ 
   const darthVaderObject = JSON.parse(this.responseText)
-  document.getElementById('persons4name').innerHTML = darthVaderObject.name
+  document.getElementById('persons4Name').innerHTML = darthVaderObject.name;
 });
+
+let darthVaderHome = new XMLHttpRequest ();
+darthVaderHome.open('GET', "https://swapi.co/api/planets/1/")
+darthVader.send();
+
+darthVaderHome.addEventListener('load', function () {
+  const darthVaderObject = JSON.parse(this.responseText).innerHTML = darthVaderObject.name;
+  document.getElementById('person4HomeWorld')
+});
+
+
