6b0198de2d43cf8353827f89244a0c2f8d31d08e
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-13 14:06:29 -1000

    closer

--- app.js/app.js
+++ app.js/app.js
@@ -1,3 +1,8 @@
 let darthVader = new XMLHttpRequest ();
 darthVader.open('GET', 'https://swapi.co/api/people/4/')
 darthVader.send();
+
+darthVader.addEventListener('load', function (){ 
+  const darthVaderObject = JSON.parse(this.responseText)
+  document.getElementById('persons4name').innerHTML = darthVaderObject.name
+});
